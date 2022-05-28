b310fe4c3303dd406bfb31464fa80ecac1651853
Leonardo de Araújo Lima <leonardo@deb-power.asl-sl.net>
2022-05-28 13:39:26 -0300

    updated dbconvert docs/usage

--- dbconvert/dump.sql
+++ dbconvert/dump.sql
@@ -1,7 +1,6 @@
 DROP TABLE IF EXISTS equipe;
 CREATE TABLE IF NOT EXISTS `equipe`( id integer , nome text , sobrenome text , email text  );
 
-INSERT INTO equipe VALUES(0,`nome`,`sobrenome`,`email`);
 
-INSERT INTO equipe VALUES(1,'teste','asl-sl','teste@asl-sl.com.br');
+INSERT INTO equipe VALUES(0,'teste','asl-sl','teste@asl-sl.com.br');
 
